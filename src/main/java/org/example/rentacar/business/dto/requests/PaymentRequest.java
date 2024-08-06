a6ff0f9a72995732dd49c71c39738b5d9371fbd1
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2024-08-06 23:47:49 +0300

    .

--- src/main/java/org/example/rentacar/business/dto/requests/PaymentRequest.java
+++ src/main/java/org/example/rentacar/business/dto/requests/PaymentRequest.java
@@ -17,18 +17,25 @@ import org.hibernate.validator.constraints.Length;
 public class PaymentRequest {
     @Min(value = 2023)
     private int CardExpirationYear;
+
     @Max(value = 12)
     @Min(value = 1)
     private int CardExpirationMonth;
+
     @NotBlank(message = "Card Number is not validated.")
     @Length(min = 16, max = 16, message = "Must be 16 characters long.")
     private String CardNumber;
+
     @NotBlank
     @Length(min = 5)
     private String cardHolder;
+
     @NotNull
     @NotBlank
     @Length(min = 3, max = 3)
     private String cardCvv;
 
+
 }
+
+
--- src/main/java/org/example/rentacar/business/dto/requests/create/CreatePaymentRequest.java
+++ src/main/java/org/example/rentacar/business/dto/requests/create/CreatePaymentRequest.java
@@ -16,4 +16,3 @@ public class CreatePaymentRequest extends PaymentRequest {
     private double balance;
 
 }
-
